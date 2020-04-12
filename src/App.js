435fda2de101139d6980042cd049ab81790bf8b5
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-04-12 10:30:07 +0200

    Create Basic Html Elements

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        <div className='w-75 mx-auto text-center d-flex flex-column'>
+          <h2>Remainder Tasks</h2>
+          <input type='text' className='form-control mt-3' placeholder='What Should You Do ...'/>
+          <input type='datetime-local' className='form-control my-3'/>
+          <button className='btn btn-primary'>Add Task</button>
+          <button className='btn btn-danger mt-2'>Clear</button>
+        </div>
       </div>
     );
   }
