8233c8448436aa74db761a2e973f84100a0a1651
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-04-12 10:47:03 +0200

    1- Build Redux Requirements ----------------------   Step1 : Create Action Types   Step2 : Create Action Creator   Step3 : Create Reducer
    Step1 : Create Action Types File Called [actionTypes.js]
    
    Step2 : Create Action Creators Folder [actionCreators.js]

---
+++ src/actionCreators/ActionCreators.js
@@ -0,0 +1,9 @@
+import {addRemainder} from '../actionTypes'
+
+export const add_Remainder = (text,date)=>{
+  const action = {
+    type: addRemainder,
+    text,
+    date
+  }
+}
---
+++ src/actionTypes.js
@@ -0,0 +1,1 @@
+export const addRemainder = 'ADD_REMAINDER'
