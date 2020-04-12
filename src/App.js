07e65914a498d37c463243985b655a3339752f70
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-04-12 10:07:41 +0200

    Setup Redux And Bootstrap 1- Install Redux 2- Install Bootstrap 3- Create Redux Reducer Files And Import It In [index.js]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
